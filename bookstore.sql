15c0f21555a57cd772e4a9119f53f037ec8ea820
hong <j520123456789@gmail.com>
2023-05-26 18:19:38 +0800

    更新資料庫的內容:D

--- bookstore.sql
+++ bookstore.sql
@@ -28,16 +28,17 @@ CREATE TABLE `members` (
   `member_id` int NOT NULL AUTO_INCREMENT,
   `member_name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `password` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `phone_number` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `county` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `district` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `address` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `password` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `gender` varchar(6) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `phone_number` varchar(10) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `county` varchar(10) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `district` varchar(10) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `address` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `birthday` date DEFAULT NULL,
   `registration_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`member_id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -46,7 +47,7 @@ CREATE TABLE `members` (
 
 LOCK TABLES `members` WRITE;
 /*!40000 ALTER TABLE `members` DISABLE KEYS */;
-INSERT INTO `members` VALUES (1,'李欣樺','11044106@cycu.edu.tw','11044106','0912345678','桃園市','中壢區','中北路200號','1990-01-01','2023-05-20 11:55:03'),(2,'辜麗慈','11044114@cycu.edu.tw','11044114','0912345678','桃園市','中壢區','中北路200號','1990-01-01','2023-05-20 11:55:03'),(3,'陳威宏','11044144@cycu.edu.tw','11044144','0912345678','桃園市','中壢區','中北路200號','2002-09-28','2023-05-20 11:55:03'),(4,'張三','zhangsan@example.com','123456','0912345678','台北市','中山區','忠孝東路一段','1990-01-01','2023-05-20 11:55:03'),(5,'李四','lisi@example.com','abcdef','0923456789','新北市','板橋區','民生路','1992-02-02','2023-05-20 11:55:03'),(6,'王五','wangwu@example.com','password123','0934567890','台中市','西屯區','市政北一路','1995-03-03','2023-05-20 11:55:03'),(7,'陳六','chenliu@example.com','qwerty','0945678901','高雄市','鳳山區','鳳北路','1998-04-04','2023-05-20 11:55:03'),(8,'劉七','liuqi@example.com','654321','0956789012','台南市','安南區','安中路','2000-05-05','2023-05-20 11:55:03'),(9,'陳威宏','j520123456789@gmail.com','1234','0906686256','桃園市','楊梅區','梅獅路二段232巷60號7樓','2023-05-03','2023-05-20 13:03:18');
+INSERT INTO `members` VALUES (1,'李欣樺','11044106@cycu.edu.tw','11044106','female','0912345678','桃園市','中壢區','中北路200號','1990-01-01','2023-05-24 07:40:33'),(2,'辜麗慈','11044114@cycu.edu.tw','11044114','female','0912345678','桃園市','中壢區','中北路200號','1990-01-01','2023-05-24 07:40:33'),(3,'陳威宏','11044144@cycu.edu.tw','11044144','male','0912345678','桃園市','中壢區','中北路200號','2002-09-28','2023-05-24 07:40:33'),(4,'張三','zhangsan@example.com','123456','male','0912345678','台北市','中山區','忠孝東路一段','1990-01-01','2023-05-24 07:40:33'),(5,'李四','lisi@example.com','abcdef','female','0923456789','新北市','板橋區','民生路','1992-02-02','2023-05-24 07:40:33'),(6,'王五','wangwu@example.com','password123','male','0934567890','台中市','西屯區','市政北一路','1995-03-03','2023-05-24 07:40:33'),(7,'陳六','chenliu@example.com','qwerty','female','0945678901','高雄市','鳳山區','鳳北路','1998-04-04','2023-05-24 07:40:33'),(8,'劉七','liuqi@example.com','654321','female','0956789012','台南市','安南區','安中路','2000-05-05','2023-05-24 07:40:33');
 /*!40000 ALTER TABLE `members` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -59,4 +60,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-05-20 22:49:53
+-- Dump completed on 2023-05-26 18:17:19
