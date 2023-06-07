54f1c0bf49bc44190d679ab6f5bad651d07229c4
XIN-HUA-LI <christinelee1114@gmail.com>
2023-06-08 00:00:10 +0800

    加入點閱分析的view跟Stored procedure

--- bookstore_edit.sql
+++ bookstore_edit.sql
@@ -38,3 +38,106 @@ INSERT INTO about VALUE ('6', '陳威宏', '秉持著大一從做中學的精神
 後端的核心在於實現商業邏輯，果然邏輯這件事情是很難建立關聯的。在自學與不斷收到error:500的錯誤中不斷嘗試與思考究竟要做到什麼地步，
 好像怎麼寫都對又好像怎麼寫都錯，卻又不甘於算了，還是方便就好。非常感謝組員們用盡了耐心彼此交換意見，讓這份專案能夠順利完工。');
 
+
+#合併產品表與留言板＝會員評論紀錄
+DELIMITER $$
+DROP PROCEDURE IF EXISTS member_comment; $$
+CREATE PROCEDURE member_comment (IN memberName varchar(100))
+BEGIN
+    select products.img, board.comments_date, products.product_name, board.star, board.comments
+    from board
+    join products on(board.product_id=products.product_id)
+    where board.member_name=memberName
+    order by comments_date desc;
+END $$
+DELIMITER ;
+
+#建立以性別分類的各類型點閱人數 view
+DROP VIEW IF EXISTS click_gender_view;
+CREATE VIEW click_gender_view AS
+SELECT
+	members.gender,
+	SUM(CASE WHEN click.horry_count > 0 THEN 1 ELSE 0 END) as horry_nop, 
+    SUM(CASE WHEN click.love_count > 0 THEN 1 ELSE 0 END) as love_nop,
+    SUM(CASE WHEN click.suspense_count > 0 THEN 1 ELSE 0 END) as suspense_nop,
+    SUM(CASE WHEN click.fantasy_count > 0 THEN 1 ELSE 0 END) as fantasy_nop
+FROM click , members
+where click.member_id = members.member_id
+GROUP BY gender;
+
+#查詢某類型某性別的點閱人數
+DELIMITER $$
+DROP PROCEDURE IF EXISTS click_gender_nop; $$
+CREATE PROCEDURE click_gender_nop (類型 varchar(10),性別 VARCHAR(6))
+BEGIN
+	select
+	CASE
+		WHEN 類型='horry' THEN (select horry_nop from click_gender_view where gender=性別)
+		WHEN 類型='love' THEN (select love_nop from click_gender_view where gender=性別)
+        WHEN 類型='suspense' THEN (select suspense_nop from click_gender_view where gender=性別)
+        WHEN 類型='fantasy' THEN (select fantasy_nop from click_gender_view where gender=性別)
+		ELSE 0 
+    end as 此類型此性別的點閱人數
+    from click_gender_view
+    where gender=性別;    
+END $$
+DELIMITER ;
+
+
+#建立以性別分類的各類型點閱人數 view
+ # 此查詢使用 LEFT JOIN 將年齡層列表 (age_groups) 與點擊數據 (click) 進行連接，以包含所有年齡層，即使在點擊數據中沒有對應的記錄。然後，使用 IFNULL 函數將不存在的點擊數設為 0。
+DROP VIEW IF EXISTS click_age_group_view;
+CREATE VIEW click_age_group_view AS
+SELECT
+    age_groups.age_group,
+    IFNULL(click.horry_nop, 0) as horry_nop,
+    IFNULL(click.love_nop, 0) as love_nop,
+    IFNULL(click.suspense_nop, 0) as suspense_nop,
+    IFNULL(click.fantasy_nop, 0) as fantasy_nop
+FROM
+    (SELECT '0-10' AS age_group
+     UNION SELECT '11-20'
+     UNION SELECT '21-30'
+     UNION SELECT '31-40'
+     UNION SELECT '41-50'
+     UNION SELECT '51-60'
+     UNION SELECT '61-70'
+     UNION SELECT 'over 71') AS age_groups
+LEFT JOIN
+    (SELECT
+        CASE
+            WHEN TIMESTAMPDIFF(YEAR, members.birthday, CURDATE()) BETWEEN 0 AND 10 THEN '0-10'
+            WHEN TIMESTAMPDIFF(YEAR, members.birthday, CURDATE()) BETWEEN 11 AND 20 THEN '11-20'
+            WHEN TIMESTAMPDIFF(YEAR, members.birthday, CURDATE()) BETWEEN 21 AND 30 THEN '21-30'
+            WHEN TIMESTAMPDIFF(YEAR, members.birthday, CURDATE()) BETWEEN 31 AND 40 THEN '31-40'
+            WHEN TIMESTAMPDIFF(YEAR, members.birthday, CURDATE()) BETWEEN 41 AND 50 THEN '41-50'
+            WHEN TIMESTAMPDIFF(YEAR, members.birthday, CURDATE()) BETWEEN 51 AND 60 THEN '51-60'
+            WHEN TIMESTAMPDIFF(YEAR, members.birthday, CURDATE()) BETWEEN 61 AND 70 THEN '61-70'
+            ELSE 'over 71'
+        END AS age_group,
+        SUM(CASE WHEN click.horry_count > 0 THEN 1 ELSE 0 END) as horry_nop, 
+        SUM(CASE WHEN click.love_count > 0 THEN 1 ELSE 0 END) as love_nop,
+        SUM(CASE WHEN click.suspense_count > 0 THEN 1 ELSE 0 END) as suspense_nop,
+        SUM(CASE WHEN click.fantasy_count > 0 THEN 1 ELSE 0 END) as fantasy_nop
+    FROM click , members
+    WHERE click.member_id = members.member_id
+    GROUP BY age_group) AS click ON age_groups.age_group = click.age_group;
+
+#查詢某類型某年齡層的點閱人數
+DELIMITER $$
+DROP PROCEDURE IF EXISTS click_age_group_nop; $$
+CREATE PROCEDURE click_age_group_nop (book_type varchar(10),age varchar(7))
+BEGIN
+	select
+	CASE
+		WHEN book_type='horry' THEN (select horry_nop from click_age_group_view where age_group like concat('%', age, '%'))
+		WHEN book_type='love' THEN (select love_nop from click_age_group_view where age_group like concat('%', age, '%'))
+        WHEN book_type='suspense' THEN (select suspense_nop from click_age_group_view where age_group like concat('%', age, '%'))
+        WHEN book_type='fantasy' THEN (select fantasy_nop from click_age_group_view where age_group like concat('%', age, '%'))
+		ELSE '0' 
+    end as 此類型此年齡層的點閱人數
+    from click_age_group_view
+    where age_group like age;    
+END $$
+DELIMITER ;
+
